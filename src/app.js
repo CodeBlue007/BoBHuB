43f91aeb0be4f4220d63604058247b4d235bfc5b
상준 안 <ahosang0056@gmail.com>
2022-12-13 15:32:31 +0900

    update: package setting

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import cors from "cors";
+import express from "express";
+
+import { errorLogger, errorHandler } from "./middlewares";
+
+const app = express();
+
+// CORS 에러 방지
+app.use(cors());
+
+// Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.json());
+
+// Content-Type: application/x-www-form-urlencoded 형태의 데이터를
+// 인식하고 핸들링할 수 있게 함.
+app.use(express.urlencoded({ extended: false }));
+
+// 미들웨어 (에러를 error.log 파일에 기록 및, 에러를 프론트엔드에 전달)
+app.use(errorLogger);
+app.use(errorHandler);
+
+export { app };
