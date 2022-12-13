67836762e6d375a2f915059106bbe1ef04eacee9
dejaikeem <dejaikeem@gmail.com>
2022-12-13 15:38:59 +0900

    update: Set directory

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  userRouter,
+  shopRouter,
+  groupRouter,
+  categoryRouter,
+  commentRouter,
+  authRouter,
+  adminRouter,
+} from "./routers";
+
+
+export { app };
