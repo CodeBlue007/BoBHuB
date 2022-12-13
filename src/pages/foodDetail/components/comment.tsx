8cfeb637b15c3689c7f74c2f06072e86c2b663c3
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-13 20:39:35 +0900

    Update(food-Detail) : 임시데이터 추가

---
+++ src/pages/foodDetail/components/comment.tsx
@@ -0,0 +1,43 @@
+export const comment = [
+  {
+    commnetId: 1,
+    userId: 12345,
+    shopId: 45678,
+    content: '댓글내용입니다',
+    star: 1,
+  },
+  {
+    commnetId: 2,
+    userId: 1523,
+    shopId: 1561532,
+    content: '댓글내용입니다',
+    star: 2,
+  },
+  {
+    commnetId: 3,
+    userId: 1231635,
+    shopId: 45613561,
+    content: '댓글내용입니다',
+    star: 5,
+  },
+  {
+    commnetId: 4,
+    userId: 1515618594,
+    shopId: 152359,
+    content: '댓글내용입니다',
+    star: 4,
+  },
+];
+
+export const shop = {
+  shopId: 12345,
+  categoryId: 2,
+  name: '식당이름',
+  distance: 1000,
+  address: '식당위치',
+  menu: '메뉴',
+  shopPicture: '식당사진',
+  like: 100,
+  description: 
+  '식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명식당설명',
+};
