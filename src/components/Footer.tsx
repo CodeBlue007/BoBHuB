8fec4999943dedbb59fa04db5515f475423cb7b8
Yoo Jeong KIm <jeongola@naver.com>
2022-12-13 16:57:04 +0900

    feature/FE/MainPage : IntroSection layout fixed

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+const footer = () => {
+  return;
+};
+
+export default footer;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,24 @@
 import NavBar from '../../components/NavBar';
 import IntroSection from './components/IntroSection';
 import SliderSection from './components/SliderSection';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  height: 300vh;
+  width: 100vw:
+  min-width: 1000px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
 
 const MainPage = () => {
   return (
-    <div>
+    <Content>
       <NavBar />
       <IntroSection />
       <SliderSection />
-    </div>
+    </Content>
   );
 };
 
