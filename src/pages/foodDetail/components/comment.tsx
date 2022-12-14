5dd76020cdb016d29bc415a265f211624cddfabd
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-14 11:22:03 +0900

    Update(foodDetail) : Comment 컴포넌트 별점 추가

--- src/pages/foodDetail/components/comment.tsx
+++ src/pages/foodDetail/components/comment.tsx
@@ -1,25 +1,44 @@
 import styled from 'styled-components';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, Typography, Rating } from '@mui/material';
 
 const CommentContainer = styled.div`
   display: flex;
+  width: 50vw;
+  position: relative;
+`;
+
+const RatingContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top : -35px;
+  left : 10px;
+`;
+
+const CommentField = styled(TextField)`
+  width:40vw;
 `;
 
 const Comment = () => {
-
   return (
     <CommentContainer>
-      <TextField
+      <RatingContainer>
+        <Typography component="legend">식당은 어땠나요?</Typography>
+        <Rating
+          name="simple-controlled"
+          value={2}
+          // onChange={(event, newValue) => {
+          //   setValue(newValue);
+          // }}
+        />
+      </RatingContainer>
+      <CommentField
         id="outlined-basic"
         label="댓글을 입력하세요"
         variant="outlined"
-        sx={{
-          width: 600,
-        }}
       />
       <Button variant="outlined">Enter</Button>
     </CommentContainer>
   );
-}
+};
 
 export default Comment;
