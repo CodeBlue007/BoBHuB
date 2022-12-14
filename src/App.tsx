7e3452bb89dc0b3bdd1945be3b2382b40d40f6b8
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2022-12-14 21:13:40 +0900

    Refactor(Admin-user): 안쓰는 파일, 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import SideBar from './pages/Admin/components/SideBar';
 import { GlobalStyle } from './styles/GlobalStyle';
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <SideBar />
     </div>
   );
 }
