7ba601a3024976dda31dc21145f4f9cbf522ba4a
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-15 14:36:46 +0900

    Update : 댓글생성 기능추가

--- src/pages/foodDetail/components/Comment.tsx
+++ src/pages/foodDetail/components/Comment.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 import { TextField, Button, Typography, Rating } from '@mui/material';
+import React, { useState } from 'react';
+import { commentStateProps } from '../types/Type';
 
-const CommentContainer = styled.div`
+
+const CommentContainer = styled.form`
   display: flex;
   width: 47vw;
   justify-content: space-between;
@@ -14,7 +17,7 @@ const RatingContainer = styled.div`
   top : -35px;
   left : 10px;
 `;
-
+ 
 const CommentField = styled(TextField)`
   width:40vw;
 `;
@@ -23,30 +26,56 @@ const CommentField = styled(TextField)`
 interface commnetProps{
   starValue : number|null;
   setStarValue : (x:number|null) => void;
-  content : string;
-  setContent : (x:string) => void;
+  setCommentState : React.Dispatch<React.SetStateAction<commentStateProps[]>>;
 }
 
-const Comment = ({starValue,setStarValue,content, setContent} : commnetProps) => {
+type createCommentType = React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
+
+const Comment = ({starValue,setStarValue,setCommentState} : commnetProps) => {
+
+  const [content, setContent] = useState<string>("");
+
+  const ratingChange = (e:React.SyntheticEvent, newValue:number|null) => setStarValue(newValue);
+  const fieldChange = (e:React.ChangeEvent<HTMLInputElement>) => setContent(e.target.value);
+
+  const createComment = (e:createCommentType) =>{
+    e.preventDefault();
+
+    if(content === ''){
+      alert("댓글을 입력해주세요");
+      return;
+    }
+    const newComment = {
+      commentId : 1232,
+      userId: 123465,
+      shopId : 12313,
+      star : starValue,
+      content,
+    }
+
+    setCommentState((current) => [...current, newComment]);
+    setContent('');
+  }
+
 
   return (
-    <CommentContainer>
+    <CommentContainer onSubmit={createComment}>
       <RatingContainer>
         <Typography component="legend">식당은 어땠나요?</Typography>
         <Rating
           name="simple-controlled"
           value={starValue}
-          onChange={(event:React.SyntheticEvent, newValue:number|null) => {
-            setStarValue(newValue);
-          }}
+          onChange={ratingChange}
         />
       </RatingContainer>
       <CommentField
         id="outlined-basic"
         label="댓글을 입력하세요"
         variant="outlined"
+        value={content}
+        onChange={fieldChange}
       />
-      <Button variant="outlined">Enter</Button>
+      <Button variant="outlined" onClick={createComment}>Enter</Button>
     </CommentContainer>
   );
 };
