77b3f80dce1e8532614e80214c64490e27ca7ca4
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-15 16:38:33 +0900

    Update : TextArea 댓글창 상태구현

--- src/pages/foodDetail/components/CommentList.tsx
+++ src/pages/foodDetail/components/CommentList.tsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { Avatar, Typography, Rating, Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
+import { useEffect, useState } from 'react';
+import TextArea from "./TextArea"
 
 const FlexContainer = styled.div`
   display: flex;
@@ -15,28 +17,28 @@ const ListContainer = styled(FlexContainer)`
   border-radius: 10px;
   background-color: crimson;
   position: relative;
-  margin : 15px;
+  margin: 15px;
 `;
 
 const ContentContainer = styled.div`
-  position : relative;
+  position: relative;
   display: flex;
   flex-direction: column;
   flex: 3;
   color: white;
   padding: 10px;
 
-  &>article{
+  & > article {
     margin: 10px 0;
   }
 
-  .buttonWrap{
-    display :flex;
+  .buttonWrap {
+    display: flex;
     width: 175px;
     justify-content: space-between;
-    position : absolute;
-    right :50px;
-    top : 5px;
+    position: absolute;
+    right: 20px;
+    top: 15px;
   }
 `;
 
@@ -46,8 +48,9 @@ const AvatarContainer = styled(FlexContainer)`
 `;
 
 const CustomButton = styled(Button)`
-width: 80px;
+  width: 80px;
 `;
+
 
 interface CommentList {
   commentProp: {
@@ -55,14 +58,22 @@ interface CommentList {
     userId: number;
     shopId: number;
     content: string;
-    star: number|null;
+    star: number | null;
   };
 }
-
 
 const CommentList = ({
   commentProp: { commentId, userId, shopId, content, star },
 }: CommentList) => {
+  const [textValue, setTextValue] = useState<string>('');
+  const [canRevise ,setRevise] = useState(false);
+
+  console.log(textValue);
+
+  useEffect(()=> {
+    setTextValue(content);
+  },[])
+
   return (
     <>
       <ListContainer>
@@ -72,14 +83,18 @@ const CommentList = ({
         <ContentContainer>
           <Typography component="legend">{userId}</Typography>
           <Rating name="read-only" value={star} readOnly />
-          <article>{content}</article>
+          <TextArea content={content} setTextValue={setTextValue} canRevise={canRevise} setRevise={setRevise}/>
           <div className="buttonWrap">
-          <CustomButton variant="contained" color="secondary" size="small" startIcon={<CreateIcon />}>
-            수정
-          </CustomButton>
-          <CustomButton variant="contained" color="error" size="small" startIcon={<DeleteIcon />}>
-            삭제
-          </CustomButton>
+            <CustomButton
+              variant="contained"
+              color="secondary"
+              size="small"
+              startIcon={<CreateIcon />}>
+              수정
+            </CustomButton>
+            <CustomButton variant="contained" color="error" size="small" startIcon={<DeleteIcon />}>
+              삭제
+            </CustomButton>
           </div>
         </ContentContainer>
       </ListContainer>
