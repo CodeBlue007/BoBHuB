32b279acd73307d63b7019ef7b56de648faa9fba
Yoo Jeong KIm <jeongola@naver.com>
2022-12-16 02:47:54 +0900

    Update: slider effect added

--- src/pages/MainPage/components/SliderSection.tsx
+++ src/pages/MainPage/components/SliderSection.tsx
@@ -1,28 +1,27 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import { Component } from 'react';
+import { Button } from '@mui/material';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import img_1 from '../../../assets/images/img_1.jpg';
-import img_2 from '../../../assets/images/img_2.jpg';
-import img_3 from '../../../assets/images/img_3.jpg';
-import img_4 from '../../../assets/images/img_4.jpg';
-import img_5 from '../../../assets/images/img_5.jpg';
-import img_6 from '../../../assets/images/img_6.jpg';
-import img_7 from '../../../assets/images/img_7.jpg';
-import img_8 from '../../../assets/images/img_8.jpg';
-import img_9 from '../../../assets/images/img_9.jpg';
-import img_10 from '../../../assets/images/img_10.jpg';
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
+import { data } from './data';
+
+const StyledSlider = styled(Slider)`
+  height: 300px;
+`;
 
 const Div = styled.div`
   width: 95%;
   height: 500px;
-  background-color: skyblue;
+  background-color: rgba(132, 168, 0);
+  border-radius: 20px;
   position: absolute;
   top: 800px;
   left: 10px;
   right: 10px;
   margin: 20px;
+  place-items: center;
   .slick-prev:before {
     opaicty: 1;
     color: black;
@@ -32,78 +31,134 @@ const Div = styled.div`
     opacity: 1;
     color: black;
   }
+  .slick-slide {
+    padding: 10px;
+    background-color: white;
+    margin-right: 15px;
+    border-radius: 15px;
+    height: 350px;
+    text-align: center;
+  }
+  .slide {
+    opacity: 0.5;
+    transform: scale(0.7);
+    transition: 0.3s;
+    filter: blur (5px);
+  }
+  .slide-active {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  .arrow {
+    font-size: 12px;
+    padding: 5px 15px;
+    border-radius: 10px;
+    box-shadow: 0 0 5px 3px #ccc;
+    width: 10px;
+    position: absolute;
+    top: 200px;
+    background-color: grey;
+  }
+
+  .arrow-right {
+    right: 15px;
+  }
+
+  .arrow-left {
+    left: 15px;
+    z-index: 10;
+  }
 
   img {
-    padding: 20px;
-    object-fit: filled;
-    height: 400px;
+    margin: auto auto 50px auto;
+    max-height: 200px;
+    overflow: hidden;
+    width: 100%;
+    z-index: -1;
+  }
+
+  span {
+    position: absolute;
+    top: 150px;
+    color: white;
+    font-size: 2rem;
+    font-weight: bold;
   }
 `;
 
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      pauseOnHover: true,
-      draggable: true,
-      responsive: [
-        {
-          breakpoint: 960,
-          settings: {
-            slidesToShow: 3,
-          },
+const TitleBox = styled.div`
+  height: 30px;
+  font-size: 2em;
+  margin: 30px 30px 30px 30px;
+  color: white;
+  font-weight: bold;
+`;
+
+function NextArrow({ onClick }: any) {
+  return (
+    <div className="arrow arrow-right" onClick={onClick}>
+      <AiOutlineArrowRight />
+    </div>
+  );
+}
+
+function PrevArrow({ onClick }: any) {
+  return (
+    <div className="arrow arrow-left" onClick={onClick}>
+      <AiOutlineArrowLeft />
+    </div>
+  );
+}
+
+export default function SimpleSlider() {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    draggable: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current: any, next: any) => setSlideIndex(next),
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 3,
         },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-          },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
         },
-      ],
-    };
-    return (
-      <Div>
-        <h2>오늘 뭐 먹지?</h2>
-        <Slider {...settings}>
-          <div>
-            <img src={img_1} alt="img_1" />
-          </div>
-          <div>
-            <img src={img_2} alt="img_2" />
-          </div>
-          <div>
-            <img src={img_3} alt="img_3" />
-          </div>
-          <div>
-            <img src={img_4} alt="img_4" />
-          </div>
-          <div>
-            <img src={img_5} alt="img_5" />
-          </div>
-          <div>
-            <img src={img_6} alt="img_6" />
-          </div>
-          <div>
-            <img src={img_7} alt="img_7" />
-          </div>
-          <div>
-            <img src={img_8} alt="img_8" />
-          </div>
-          <div>
-            <img src={img_9} alt="img_9" />
-          </div>
-          <div>
-            <img src={img_10} alt="img_10" />
-          </div>
-        </Slider>
-      </Div>
-    );
-  }
+      },
+    ],
+  };
+
+  return (
+    <Div>
+      <TitleBox>오늘 뭐 먹지?</TitleBox>
+      <div>
+        <StyledSlider {...settings}>
+          {data.map((menu, index) => (
+            <div className={index === slideIndex ? 'slide slide-active' : 'slide'}>
+              <img src={menu.img} alt="img" />
+              <span>{menu.restaurant}</span>
+              <Button variant="contained">찜하기</Button>
+            </div>
+          ))}
+        </StyledSlider>
+      </div>
+    </Div>
+  );
 }
