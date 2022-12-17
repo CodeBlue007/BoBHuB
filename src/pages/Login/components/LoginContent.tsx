c252d6775216d51efafdc874733a789f1207b4eb
dongjooyun <dongjooyun@khu.ac.kr>
2022-12-18 05:09:36 +0900

    Remove: loginContent 주석 제거

--- src/pages/Login/components/LoginContent.tsx
+++ src/pages/Login/components/LoginContent.tsx
@@ -1,12 +1,8 @@
 import LoginForm from './LoginForm';
-// import validateLogin from '../../../util/validateID';
 import axios from 'axios';
 
 const LoginContent = () => {
-  const onLoginSubmit = (loginForm: { id: string; password: string }) => {
-    // if (!validateLogin(loginForm)) return;
-    // console.log('login validation success');
-  };
+  const onLoginSubmit = (loginForm: { id: string; password: string }) => {};
   return <LoginForm onLoginSubmit={onLoginSubmit} />;
 };
 
