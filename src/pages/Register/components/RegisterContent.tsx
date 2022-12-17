0931969a074686f5a53fbaa60a2c0f448f0856c8
dongjooyun <dongjooyun@khu.ac.kr>
2022-12-17 22:14:19 +0900

    Update: 트랙기수 제외 validation

--- src/pages/Register/components/RegisterContent.tsx
+++ src/pages/Register/components/RegisterContent.tsx
@@ -1,7 +1,22 @@
 import RegisterForm from './RegisterForm';
+import valiDateData from '../../../util/validateRegister';
+import axios from 'axios';
 
 const RegisterContent = () => {
-  return <RegisterForm />;
+  const onRegSubmit = (regForm: {
+    name: string;
+    id: string;
+    nickName: string;
+    password: string;
+    passwordCheck: string;
+    phone: string;
+    email: string;
+    confirmNum: string;
+  }) => {
+    if (!valiDateData(regForm)) return;
+    console.log('validation success');
+  };
+  return <RegisterForm onRegSubmit={onRegSubmit} />;
 };
 
 export default RegisterContent;
--- src/pages/Register/components/RegisterForm.tsx
+++ src/pages/Register/components/RegisterForm.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { TextField, Button, MenuItem, Box } from '@mui/material';
+import { TextField, Button, MenuItem } from '@mui/material';
 
 const RegisterFormContainer = styled.form`
   display: flex;
@@ -24,14 +26,14 @@ const BoxContainer = styled.div`
   border-radius: 4px;
 
   height: 90vh;
-  padding: 0px 20px 50px 20px;
+  padding: 0px 20px 0px 20px;
 
   margin-bottom: 80px;
 
   & input {
     font-size: 15px;
     width: 40vw;
-    height: 4vh;
+    height: 3vh;
   }
 
   & div:nth-child(2),
@@ -82,76 +84,151 @@ const BoxContainer = styled.div`
   }
 `;
 
+const BackToLogin = styled.div`
+  margin-top: -5px;
+  margin-right: 430px;
+`;
+
 const trackNum = ['SW 3기', 'SW 4기', 'IoT 1기', 'AI 6기'];
 
-const RegisterForm = () => {
-  const submitHandler = (e: React.FormEvent) => {
+type regFormProps = {
+  onRegSubmit: (regForm: {
+    name: string;
+    id: string;
+    nickName: string;
+    password: string;
+    passwordCheck: string;
+    phone: string;
+    email: string;
+    confirmNum: string;
+  }) => void;
+};
+
+const RegisterForm = ({ onRegSubmit }: regFormProps) => {
+  // useState 방식
+  const [regForm, setRegForm] = useState({
+    name: '',
+    id: '',
+    nickName: '',
+    password: '',
+    passwordCheck: '',
+    phone: '',
+    email: '',
+    confirmNum: '',
+  });
+
+  const { name, id, nickName, password, passwordCheck, phone, email, confirmNum } = regForm;
+
+  const onTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegForm({
+      ...regForm,
+      [name]: value,
+    });
+  };
+
+  const handleRegSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    onRegSubmit(regForm);
+    // form 초기화
+    setRegForm({
+      name: '',
+      id: '',
+      nickName: '',
+      password: '',
+      passwordCheck: '',
+      phone: '',
+      email: '',
+      confirmNum: '',
+    });
   };
 
   return (
-    <RegisterFormContainer onSubmit={submitHandler}>
+    <RegisterFormContainer onSubmit={handleRegSubmit}>
       <h1>Sign Up</h1>
       <BoxContainer>
         <TextField
-          id="userName"
+          name="name"
           label="이름"
           variant="standard"
-          placeholder="이름을 입력해주세요."
+          placeholder="이름을 입력해주세요 (한글 2~6글자)."
+          value={name}
+          onChange={onTextFieldChange}
         />
 
         <TextField
-          id="userID"
+          name="id"
           label="아이디"
           variant="standard"
-          placeholder="아이디를 입력해주세요."
+          placeholder="아이디를 입력해주세요 (영문(대·소문자) 5~15글자)."
+          value={id}
+          onChange={onTextFieldChange}
+        />
+
+        <TextField
+          name="nickName"
+          label="닉네임"
+          variant="standard"
+          placeholder="닉네임을 입력해주세요 (한글·영문(대·소문자) 5~10글자)."
+          value={nickName}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           type="password"
-          id="userPW"
+          name="password"
           label="비밀번호"
           variant="standard"
-          placeholder="비밀번호를 입력해주세요."
+          placeholder="비밀번호를 입력해주세요 (8~20자리 영문·숫자 조합)."
+          value={password}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           type="password"
-          id="userRePW"
+          name="passwordCheck"
           label="비밀번호 재입력"
           variant="standard"
           placeholder="비밀번호를 다시 입력해주세요."
+          value={passwordCheck}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           type="text"
-          id="userPhoneNum"
+          name="phone"
           label="휴대폰번호"
           variant="standard"
-          placeholder="휴대폰번호를 입력해주세요."
+          placeholder="휴대폰번호를 입력해주세요 (' - ' 포함)."
+          value={phone}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           type="text"
-          id="userEmail"
+          name="email"
           label="이메일"
           variant="standard"
           placeholder="이메일을 입력해주세요."
+          value={email}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           type="text"
-          id="userNum"
+          name="confirmNum"
           label="인증번호"
           variant="standard"
           placeholder="이메일에 기재된 인증번호를 입력해주세요."
+          value={confirmNum}
+          onChange={onTextFieldChange}
         />
 
         <TextField
           id="standard-select-track"
           select
           label="트랙/기수"
-          // defaultValue="EUR"
+          defaultValue=""
           // helperText="트랙/기수를 선택해주세요."
           variant="standard">
           {trackNum.map((elem) => (
@@ -161,11 +238,13 @@ const RegisterForm = () => {
           ))}
         </TextField>
 
-        <Button variant="contained">회원가입</Button>
+        <Button variant="contained" type="submit">
+          회원가입
+        </Button>
 
-        <a href="/login" className="loginLink">
-          계정이 이미 있습니다.
-        </a>
+        <BackToLogin>
+          <Link to="/login">계정이 이미 있습니다.</Link>
+        </BackToLogin>
       </BoxContainer>
     </RegisterFormContainer>
   );
