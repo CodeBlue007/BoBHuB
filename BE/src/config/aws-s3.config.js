46674801abebbe3529ade9936fc30bd430060bd8
상준 안 <ahosang0056@gmail.com>
2022-12-19 12:32:51 +0900

    feat(image): aws s3 config

---
+++ BE/src/config/aws-s3.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  awsS3Config: {
+    region: "ap-norgheast-2",
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_ACCESS_KEY_PASSWORD,
+  },
+};
