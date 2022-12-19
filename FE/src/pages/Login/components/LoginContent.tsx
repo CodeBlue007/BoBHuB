6f621285cbdcce3a00b0ab9aff2c7d5c92163354
dongjooyun <dongjooyun@khu.ac.kr>
2022-12-20 04:04:20 +0900

    Update: 이메일 로그인 변경,  textField 아이콘 삽입

--- FE/src/pages/Login/components/LoginContent.tsx
+++ FE/src/pages/Login/components/LoginContent.tsx
@@ -2,7 +2,7 @@ import LoginForm from './LoginForm';
 import axios from 'axios';
 
 const LoginContent = () => {
-  const onLoginSubmit = (loginForm: { id: string; password: string }) => {};
+  const onLoginSubmit = (loginForm: { email: string; password: string }) => {};
   return <LoginForm onLoginSubmit={onLoginSubmit} />;
 };
 
