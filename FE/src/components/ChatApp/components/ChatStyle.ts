360b2b39c4237ea015303d76cdc9a6af3050d60e
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-19 22:52:36 +0900

    Update : ChatStyle 추가

---
+++ FE/src/components/ChatApp/components/ChatStyle.ts
@@ -0,0 +1,14 @@
+import styled, { css } from 'styled-components';
+
+export const TextCss = css`
+  width: inherit;
+  color: white;
+  text-align: center;
+  padding: 25px 0px;
+  border-bottom: 1px solid white;
+`;
+
+export const Title = styled.div`
+  font-size: 30px;
+  ${TextCss}
+`;
