f5995597f3b76bedd130ac623444943214a58ca6
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-20 15:28:38 +0900

    Update FE Socket연결추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,8 +1,12 @@
 import { GlobalStyle } from './styles/GlobalStyle';
 import Router from './Router';
 import ChatApp from './components/ChatApp/ChatApp';
+import io from 'socket.io-client';
 
 function App() {
+
+  const socket = io('http://localhost:5000');
+
   return (
     <div>
       <GlobalStyle />
