29705c15154e2d9c53ca50f3d5b619a84adc1263
dejaikeem <dejaikeem@gmail.com>
2022-12-22 01:00:13 +0900

    Update(comment): Add error filtering logics and apply ErrorFactory

--- BE/src/services/comment-service.js
+++ BE/src/services/comment-service.js
@@ -1,5 +1,6 @@
 const { commentModel } = require("../db/models");
 const buildRes = require("../utils/build-response");
+const { BadRequest, NotFound, Forbidden } = require("../utils/error-factory");
 
 class CommentService {
   constructor(commentModel) {
@@ -7,13 +8,16 @@ class CommentService {
   }
 
   async create(commentDTO) {
-    const result = await this.commentModel.create(commentDTO);
-    return buildRes("c", result);
+    try {
+      const result = await this.commentModel.create(commentDTO);
+      return buildRes("c", result);
+    } catch {
+      throw new BadRequest("Body에 작성한 내용에 오류가 있습니다.");
+    }
   }
 
   async getAllByAdmin() {
     const comments = await this.commentModel.getAll();
-
     return comments;
   }
 
@@ -23,28 +27,48 @@ class CommentService {
   }
 
   async updateByAuth(newCommentDTO, commentId) {
+    const exComment = await this.commentModel.getByCommentId(commentId);
+    console.log(exComment);
+    if (exComment.length === 0) {
+      throw new NotFound("존재하는 댓글이 없습니다.");
+    }
+
     const { userId } = newCommentDTO;
+    const isByAuth = exComment[0].userId === userId;
+    if (!isByAuth) {
+      throw new Forbidden("다른 유저의 댓글에 대한 권한이 없습니다.");
+    }
 
-    const currentComment = await this.commentModel.getByCommentId(commentId);
-    const isByAuth = currentComment[0].userId === userId;
-    if (!isByAuth) throw new Error("권한이 없습니다.");
-
-    const result = await this.commentModel.update(newCommentDTO, { commentId });
-    return buildRes("u", result);
+    try {
+      const result = await this.commentModel.update(newCommentDTO, { commentId });
+      return buildRes("u", result);
+    } catch {
+      throw new BadRequest("Body에 작성한 내용에 오류가 있습니다.");
+    }
   }
 
   async deleteByAuth(userId, commentId) {
-    const currentComment = await this.commentModel.getByCommentId(commentId);
-    const isByAuth = currentComment[0].userId === userId;
-    if (!isByAuth) throw new Error("권한이 없습니다.");
+    const exComment = await this.commentModel.getByCommentId(commentId);
+    if (exComment.length === 0) throw new NotFound("존재하는 댓글이 없습니다.");
 
-    const result = await commentModel.deleteById(commentId);
-    return buildRes("d", result);
+    const isByAuth = exComment[0].userId === userId;
+    if (!isByAuth) throw new Forbidden("다른 유저의 댓글에 대한 권한이 없습니다.");
+
+    try {
+      const result = await commentModel.deleteById(commentId);
+      return buildRes("d", result);
+    } catch {
+      throw new BadRequest("Body에 작성한 내용에 오류가 있습니다.");
+    }
   }
 
   async deleteByAdmin(commentId) {
-    const result = await commentModel.deleteById(commentId);
-    return buildRes("d", result);
+    try {
+      const result = await commentModel.deleteById(commentId);
+      return buildRes("d", result);
+    } catch {
+      throw new NotFound("존재하는 댓글이 없습니다.");
+    }
   }
 }
 
