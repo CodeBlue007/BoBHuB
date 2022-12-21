03b51d8cb0b73c4639645b1aa0014ca74fff7cc4
dejaikeem <dejaikeem@gmail.com>
2022-12-22 01:00:54 +0900

    Update(category): Add error filtering logics and apply ErrorFactory

--- BE/src/services/category-service.js
+++ BE/src/services/category-service.js
@@ -1,5 +1,6 @@
 const { categoryModel } = require("../db/models");
 const buildRes = require("../utils/build-response");
+const { BadRequest, NotFound } = require("../utils/error-factory");
 
 class CategoryService {
   constructor(categoryModel) {
@@ -7,8 +8,12 @@ class CategoryService {
   }
 
   async create(categoryDTO) {
-    const result = await this.categoryModel.create(categoryDTO);
-    return buildRes("c", result);
+    try {
+      const result = await this.categoryModel.create(categoryDTO);
+      return buildRes("c", result);
+    } catch {
+      throw new BadRequest("같은 이름의 카테고리가 이미 존재합니다.");
+    }
   }
 
   async getAll() {
@@ -16,19 +21,31 @@ class CategoryService {
     return categories;
   }
 
-  async getById(category) {
-    const getCategory = await this.categoryModel.getById(category);
-    return getCategory;
-  }
-
   async update(newCategory, category) {
-    const result = await this.categoryModel.update({ category: newCategory }, { category });
-    return buildRes("u", result);
+    if (newCategory === category) {
+      throw new BadRequest("수정할 카테고리와 기존 카테고리의 이름이 동일합니다.");
+    }
+
+    const exCategory = await this.categoryModel.getById(category);
+    if (exCategory.length === 0) {
+      throw new NotFound("존재하는 카테고리가 없습니다.");
+    }
+
+    try {
+      const result = await this.categoryModel.update({ category: newCategory }, { category });
+      return buildRes("u", result);
+    } catch {
+      throw new BadRequest("수정할 이름의 카테고리가 이미 존재합니다.");
+    }
   }
 
   async deleteById(category) {
-    const result = await this.categoryModel.deleteById(category);
-    return buildRes("d", result);
+    try {
+      const result = await this.categoryModel.deleteById(category);
+      return buildRes("d", result);
+    } catch {
+      throw new NotFound("존재하는 카테고리가 없습니다.");
+    }
   }
 }
 
