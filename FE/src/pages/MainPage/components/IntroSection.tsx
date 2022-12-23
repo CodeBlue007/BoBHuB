272b52bdea48e5de83830aa64dfdcf5e80e56d39
Yoo Jeong KIm <jeongola@naver.com>
2022-12-23 12:29:50 +0900

    Fix: intro section text container

--- FE/src/pages/MainPage/components/IntroSection.tsx
+++ FE/src/pages/MainPage/components/IntroSection.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Intro = styled.div`
   display: flex;
   flex-direction: column;
-  min-width: 550px;
+  min-width: 600px;
   height: 100%;
   position: absolute;
   top: 205px;
