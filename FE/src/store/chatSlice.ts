70c65c7f650abace7ef816b42ae61afcfee86fc2
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2022-12-23 20:49:52 +0900

    Update : Chat목록추가

--- FE/src/store/chatSlice.ts
+++ FE/src/store/chatSlice.ts
@@ -1,20 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 type ChatType = {
-    chat : string[];
+    chats : {[key : string] : string[]}
 }
 
 const initialState:ChatType = {
-    chat : []
+    chats : {}
 };
 
 export const chatSlice = createSlice({
     name: "chat",
     initialState,
     reducers : {
-        update : (state,action) =>{
-            const {payload} = action.payload;
-            state.chat.push(payload);
+        enterRoom : (state, action) => {
+            const {roomName} = action.payload;
+            if(state.chats[roomName]) return;
+            state.chats[roomName] = [];
+        },
+        updateRoom : (state,action) =>{
+            const {roomName, payload} = action.payload;
+            state.chats[roomName].push(payload);
+        },
+        check : (state) =>{
+            return {...state};
         }
     }
 })
