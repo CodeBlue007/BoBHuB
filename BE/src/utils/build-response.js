b3f6c2b6de3ef6839fbba09495a6f10a452782ed
dejaikeem <dejaikeem@gmail.com>
2022-12-24 02:48:28 +0900

    Update(error): Change error handling class and logic

--- BE/src/utils/build-response.js
+++ BE/src/utils/build-response.js
@@ -7,19 +7,17 @@ module.exports = (method, result) => {
         if (result.insertId) {
           res.insertId = result.insertId;
         }
-      } else throw new Error("영향받는 데이터가 없습니다.");
+      }
       return res;
     case "u":
       if (result.changedRows >= 1) {
         res.message = "업데이트에 성공하였습니다.";
-      } else throw new Error("영향받는 데이터가 없습니다.");
-
+      }
       return res;
     case "d":
       if (result.affectedRows >= 1) {
         res.message = "삭제에 성공하였습니다.";
-      } else throw new Error("영향받는 데이터가 없습니다.");
-
+      }
       return res;
   }
 };
