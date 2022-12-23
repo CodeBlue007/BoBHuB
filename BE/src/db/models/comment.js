7c31388e4fbb2a5dc28d68dd59036cf64761d278
dejaikeem <dejaikeem@gmail.com>
2022-12-24 03:31:49 +0900

    Update(comment): Change error handling process as new error handling logic is changed

--- BE/src/db/models/comment.js
+++ BE/src/db/models/comment.js
@@ -1,21 +1,39 @@
 const { pool } = require("../mysql-pool");
 const o = new (require("../../utils/build-query"))("comment");
+const buildRes = require("../../utils/build-response");
+const { ErrorFactory, commonErrors } = require("../../utils/error-factory");
 
 class CommentModel {
   async create(commentDTO) {
-    const { keyArr, valArr } = o.objToKeyValueArray(commentDTO);
-    const query = o.makeInsertQuery(keyArr, valArr);
-    console.log(query);
+    try {
+      const { keyArr, valArr } = o.objToKeyValueArray(commentDTO);
+      const query = o.makeInsertQuery(keyArr, valArr);
+      console.log(query);
 
-    const [result] = await pool.query(query);
-    return result;
+      const [result] = await pool.query(query);
+      return buildRes("c", result);
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.BAD_REQUEST,
+        400,
+        "이미 작성한 내역이 있는 식당이거나 Body의 요청 내용이 잘못되었습니다."
+      );
+    }
   }
 
   async getAll() {
-    const query = o.makeSelectQuery();
-    console.log(query);
-    const [comments] = await pool.query(query);
-    return comments;
+    try {
+      const query = o.makeSelectQuery();
+      console.log(query);
+      const [comments] = await pool.query(query);
+      return comments;
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.DB_ERROR,
+        500,
+        "요청한 내용으로 DB에서 처리할 수 없습니다."
+      );
+    }
   }
 
   async getByShopId(shopId) {
@@ -28,37 +46,65 @@ class CommentModel {
 
       const [comments] = await pool.query(query, [shopId]);
       return comments;
-    } catch (err) {
-      throw new Error(err);
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.DB_ERROR,
+        500,
+        "요청한 내용으로 DB에서 처리할 수 없습니다."
+      );
     }
   }
 
   async getByCommentId(commentId) {
-    const whereArr = o.objToQueryArray({ commentId });
-    const query = o.makeSelectQuery(undefined, whereArr);
-    console.log(query);
+    try {
+      const whereArr = o.objToQueryArray({ commentId });
+      const query = o.makeSelectQuery(undefined, whereArr);
+      console.log(query);
 
-    const [comment] = await pool.query(query);
-    return comment;
+      const [comment] = await pool.query(query);
+      return comment;
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.DB_ERROR,
+        500,
+        "요청한 내용으로 DB에서 처리할 수 없습니다."
+      );
+    }
   }
 
   async update(newCommentDTO, commentDTO) {
-    const newDTO = o.objToQueryArray(newCommentDTO);
-    const oldDTO = o.objToQueryArray(commentDTO);
-    const query = o.makeUpdateQuery(newDTO, oldDTO);
+    try {
+      const newDTO = o.objToQueryArray(newCommentDTO);
+      const oldDTO = o.objToQueryArray(commentDTO);
+      const query = o.makeUpdateQuery(newDTO, oldDTO);
 
-    console.log(query);
-    const [result] = await pool.query(query);
-    return result;
+      console.log(query);
+      const [result] = await pool.query(query);
+      return buildRes("u", result);
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.DB_ERROR,
+        500,
+        "요청한 내용으로 DB에서 처리할 수 없습니다."
+      );
+    }
   }
 
   async deleteById(commentId) {
-    const whereArr = o.objToQueryArray({ commentId });
-    const query = o.makeDeleteQuery(whereArr);
-    console.log(query);
+    try {
+      const whereArr = o.objToQueryArray({ commentId });
+      const query = o.makeDeleteQuery(whereArr);
+      console.log(query);
 
-    const [result] = await pool.query(query);
-    return result;
+      const [result] = await pool.query(query);
+      return buildRes("d", result);
+    } catch {
+      throw new ErrorFactory(
+        commonErrors.DB_ERROR,
+        500,
+        "요청한 내용으로 DB에서 처리할 수 없습니다."
+      );
+    }
   }
 }
 
