9a84963de2537088abb86f7f2a47b430a8c8d310
Kim Chansoo <96669858+cskim9410@users.noreply.github.com>
2022-12-24 16:57:32 +0900

    Style: 최대높이변경

--- FE/src/components/MyParty.tsx
+++ FE/src/components/MyParty.tsx
@@ -47,7 +47,7 @@ const Container = styled.div<{ open: boolean }>`
   z-index: 999;
   box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
     rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
-  max-height: ${({ open }) => (open ? '600px' : 0)};
+  max-height: ${({ open }) => (open ? '500px' : 0)};
   opacity: ${({ open }) => (open ? 1 : 0)};
   transition: all 0.3s ease-in-out;
   overflow: hidden;
@@ -84,7 +84,7 @@ const Close = styled(CloseIcon)`
 `;
 
 const ListWrapper = styled(Div)`
-  max-height: 600px;
+  max-height: 400px;
   overflow-y: auto;
 `;
 
