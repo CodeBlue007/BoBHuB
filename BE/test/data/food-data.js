a1cde47d1acba28a90828b0d2616484322921782
dejaikeem <dejaikeem@gmail.com>
2022-12-27 02:53:54 +0900

    Update(image): Upload shop and food image of korean and chinese

--- BE/test/data/food-data.js
+++ BE/test/data/food-data.js
@@ -80,124 +80,285 @@ exports.힘톤씨의주방 = [
   },
 ];
 exports.샌드위밋 = [
-    {
-      shopId: "",
-      picture: "",
-      name: "햄치즈 샌드위치",
-      price: 7900,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "미트볼치즈 샌드위치",
-      price: 8900,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "B.L.A.T 샌드위치",
-      price: 8900,
-    },
-  ];
+  {
+    shopId: "",
+    picture: "",
+    name: "햄치즈 샌드위치",
+    price: 7900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "미트볼치즈 샌드위치",
+    price: 8900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "B.L.A.T 샌드위치",
+    price: 8900,
+  },
+];
 
 //분식 Snack
 exports.신의한국수 = [
-    {
-      shopId: "",
-      picture: "",
-      name: "야채김밥",
-      price: 3000,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "오뎅우동",
-      price: 8000,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "잔치국수",
-      price: 6000,
-    },
-  ];
-  exports.오마뎅 = [
-    {
-      shopId: "",
-      picture: "",
-      name: "물어묵",
-      price: 2000,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "떡볶이",
-      price: 4500,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "구어바",
-      price: 2200,
-    },
-  ];
-  exports.신전떡볶이 = [
-    {
-      shopId: "",
-      picture: "",
-      name: "치즈떡볶이",
-      price: 5500,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "떡볶이",
-      price: 3500,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "만두튀김(5개)",
-      price: 1700,
-    },
-  ];
-  exports.북촌손만두 = [
-    {
-      shopId: "",
-      picture: "",
-      name: "북촌튀김만두",
-      price: 6500,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "북촌피냉면",
-      price: 8500,
-    },
-    {
-      shopId: "",
-      picture: "",
-      name: "북촌만둣국",
-      price: 8500,
-    },
-  ];
-  exports.이레분식 = [
-      {
-        shopId: "",
-        picture: "",
-        name: "왕돈가스",
-        price: 8500,
-      },
-      {
-        shopId: "",
-        picture: "",
-        name: "치즈돈가스",
-        price: 8000,
-      },
-      {
-        shopId: "",
-        picture: "",
-        name: "제육덮밥",
-        price: 7000,
-      },
-    ];
+  {
+    shopId: "",
+    picture: "",
+    name: "야채김밥",
+    price: 3000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "오뎅우동",
+    price: 8000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "잔치국수",
+    price: 6000,
+  },
+];
+exports.오마뎅 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "물어묵",
+    price: 2000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "떡볶이",
+    price: 4500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "구어바",
+    price: 2200,
+  },
+];
+exports.신전떡볶이 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "치즈떡볶이",
+    price: 5500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "떡볶이",
+    price: 3500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "만두튀김(5개)",
+    price: 1700,
+  },
+];
+exports.북촌손만두 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "북촌튀김만두",
+    price: 6500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "북촌피냉면",
+    price: 8500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "북촌만둣국",
+    price: 8500,
+  },
+];
+exports.이레분식 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "왕돈가스",
+    price: 8500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "치즈돈가스",
+    price: 8000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "제육덮밥",
+    price: 7000,
+  },
+];
+//한식 Korean
+exports.보승회관 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "순대국밥",
+    price: 8500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "수육국밥",
+    price: 8500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "냉면",
+    price: 7000,
+  },
+];
+exports.두루치기김치찌개 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "두루치기김치찌개",
+    price: 8000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "오겹살",
+    price: 13000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "항정살",
+    price: 15000,
+  },
+];
+exports.완백부대찌개삼겹살 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "생꽃삼겹",
+    price: 1500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "콩나물삼겹철판",
+    price: 1200,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "완백부대찌개",
+    price: 9900,
+  },
+];
+exports.봉추찜닭 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "봉추찜닭(소)",
+    price: 23000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "봉추찜닭(중)",
+    price: 35000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "봉추찜닭(대)",
+    price: 48000,
+  },
+];
+exports.어멍네고기국수 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "고기국수",
+    price: 9000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "고기국밥",
+    price: 9000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "고사리육개장",
+    price: 9000,
+  },
+];
+exports.재희네식당 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "가정식백반",
+    price: 6000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "제육볶음",
+    price: 8000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "순두부찌개",
+    price: 7000,
+  },
+];
+exports.호호반점 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "호짜장",
+    price: 6500,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "1인세트",
+    price: 12000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "계란볶음밥",
+    price: 9000,
+  },
+];
+exports.미각 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "짜장면",
+    price: 5000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "짬뽕",
+    price: 7000,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "탕수육",
+    price: 15000,
+  },
+];
