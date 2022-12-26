35ae443031e536de8da2b8371803d84cb831efe8
dejaikeem <dejaikeem@gmail.com>
2022-12-27 00:55:01 +0900

    Chore: Shorten address link url

--- BE/test/data/shop-data.js
+++ BE/test/data/shop-data.js
@@ -4,7 +4,7 @@ exports.베이커리 = [
     name: "곤트라쉐리에",
     distance: 3,
     address:
-      "https://map.naver.com/v5/entry/place/1641731613?placePath=%2Freview%2Fvisitor&c=14144444.3281934,4515543.1698381,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1641731613",
     menu: "",
     shopPicture: "",
     description: "유명 빵집! 포장해서 지하 1층에서 먹을 수 있고 라운지에서 드셔도 돼요!",
@@ -14,7 +14,7 @@ exports.베이커리 = [
     name: "버거보이",
     distance: 3,
     address:
-      "https://map.naver.com/v5/entry/place/1263574785?placePath=%2Fhome&c=14144434.8075737,4515563.4084548,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1263574785",
     menu: "",
     shopPicture: "",
     description: "수제 버거 집인데 분위기 좋아요~ ",
@@ -24,7 +24,7 @@ exports.베이커리 = [
     name: "힘톤씨의 주방",
     distance: 5,
     address:
-      "https://map.naver.com/v5/entry/place/1647049098?placePath=%2Fhome&c=14144635.5082869,4515535.9250753,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1647049098",
     menu: "",
     shopPicture: "",
     description: "가게에서 먹을 수 있고 양이 많아요~",
@@ -34,7 +34,7 @@ exports.베이커리 = [
     name: "이삭토스트",
     distance: 10,
     address:
-      "https://map.naver.com/v5/entry/place/31699366?placePath=%2Fhome&c=14144151.3909534,4515604.0906292,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/31699366",
     menu: "",
     shopPicture: "",
     description: "이삭은 이삭이다!",
@@ -44,7 +44,7 @@ exports.베이커리 = [
     name: "샌드위밋",
     distance: 3,
     address:
-      "https://map.naver.com/v5/entry/place/1436205658?c=14144468.2173555,4515607.6568724,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1436205658",
     menu: "",
     shopPicture: "",
     description: "샌드위치에 고기(meet)들어가는 컨셉!",
@@ -56,7 +56,7 @@ exports.분식 = [
     name: "신의 한국수",
     distance: 7,
     address:
-      "https://map.naver.com/v5/entry/place/1665048009?placePath=%2Fhome&c=14144393.7686475,4515768.3985919,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1665048009",
     menu: "",
     shopPicture: "",
     description: "김밥과 국수류를 다양하게 즐기세요!",
@@ -66,7 +66,7 @@ exports.분식 = [
     name: "오마뎅",
     distance: 3,
     address:
-      "https://map.naver.com/v5/entry/place/1815005940?placePath=%2Fhome&c=14144432.3836123,4515549.4458286,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1815005940",
     menu: "",
     shopPicture: "",
     description: "추억의 분식들이 있는 집!",
@@ -76,7 +76,7 @@ exports.분식 = [
     name: "신전떡볶이",
     distance: 8,
     address:
-      "https://map.naver.com/v5/entry/place/37761464?placePath=%2Fhome&c=14144711.7844021,4515470.7505160,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/37761464",
     menu: "",
     shopPicture: "",
     description: "진리의 신전 떡볶이, 낙낙 근처에도 있다!",
@@ -86,7 +86,7 @@ exports.분식 = [
     name: "북촌손만두",
     distance: 5,
     address:
-      "https://map.naver.com/v5/entry/place/1709243455?c=14144393.7686475,4515768.3985919,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1709243455",
     menu: "",
     shopPicture: "",
     description: "여름에나 겨울에나 맛있는 메뉴들을 간단하지만 든든한 한끼로 먹을 수 있습니다~",
@@ -96,7 +96,7 @@ exports.분식 = [
     name: "이레분식",
     distance: 10,
     address:
-      "https://map.naver.com/v5/entry/place/1060846170?placePath=%2Fhome&c=14144447.7110495,4515896.7217505,15,0,0,0,dh",
+      "https://map.naver.com/v5/entry/place/1060846170",
     menu: "",
     shopPicture: "",
     description: "돈가스와 덮밥이 주 메뉴이며 간단한 국수류도 판매합니다~",
