2c976e421f9f14a6d250005f8dc793e573f9ed55
dejaikeem <dejaikeem@gmail.com>
2022-12-26 23:46:31 +0900

    Chore: Change address to link url

---
+++ BE/test/data/food-data.js
@@ -0,0 +1,103 @@
+//Bakery 제과 or 베이커리
+exports.곤트란쉐리에 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "퀸아망",
+    price: 4300,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "뺑오쇼콜라",
+    price: 4300,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "크로와상",
+    price: 3900,
+  },
+];
+exports.버거보이 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "Classic Single",
+    price: 6900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "Crispy Chicken",
+    price: 8700,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "Bigboy",
+    price: 10100,
+  },
+];
+exports.이삭토스트 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "햄치즈 토스트",
+    price: 2900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "베이컨 베스트",
+    price: 3700,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "감자 토스트",
+    price: 3500,
+  },
+];
+exports.힘톤씨의주방 = [
+  {
+    shopId: "",
+    picture: "",
+    name: "슈림프 샐러드",
+    price: 9900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "슈림프 샌드위치",
+    price: 7900,
+  },
+  {
+    shopId: "",
+    picture: "",
+    name: "치킨텐더 샌드위치",
+    price: 7900,
+  },
+];
+exports.샌드위밋 = [
+    {
+      shopId: "",
+      picture: "",
+      name: "햄치즈 샌드위치",
+      price: 7900,
+    },
+    {
+      shopId: "",
+      picture: "",
+      name: "미트볼치즈 샌드위치",
+      price: 8900,
+    },
+    {
+      shopId: "",
+      picture: "",
+      name: "B.L.A.T 샌드위치",
+      price: 8900,
+    },
+  ];
+
+//분식 Snack
