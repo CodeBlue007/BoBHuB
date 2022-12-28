2a0d54a380aa858f654f26e4685af24b0e401a88
상준 안 <ahosang0056@gmail.com>
2022-12-28 15:21:50 +0900

    feat(socket): joinParty, leaveParty 추가

--- BE/src/socket/sliderEvents.js
+++ BE/src/socket/sliderEvents.js
@@ -1,20 +1,22 @@
+const { pickService } = require("../services");
+
 module.exports = (io, socket) => {
-  const joinParty = (partyId, userId) => {
+  const joinParty = async (partyId, userId) => {
     console.log(partyId);
     console.log(userId);
     //--------------------------여기 안에다가 유저 파티에 넣어주시면 됩니당
-
+    const result = await pickService.joinParty(userId, partyId);
     //----------------------------------------------
-    io.sockets.emit("joinSuccess", "모임 참여에 성공하였습니다.");
+    io.sockets.emit("joinSuccess", result);
   };
 
-  const leaveParty = (partyId, userId) => {
+  const leaveParty = async (partyId, userId) => {
     console.log(partyId);
     console.log(userId);
     ///----------------------여기는 유저 파티에서 빼주시면 됩니다
-
+    const result = await pickService.leaveParty(userId, partyId);
     //----------------------------------------------------
-    io.sockets.emit("leaveSuccess", "모임에서 빠졌습니다.");
+    io.sockets.emit("leaveSuccess", result);
   };
 
   socket.on("joinParty", joinParty);
