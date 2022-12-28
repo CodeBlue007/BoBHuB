dfc6aac43298e3bc7a8a1727c9da4f6b4b5050b4
상준 안 <ahosang0056@gmail.com>
2022-12-28 18:16:17 +0900

    chore: dummy -> default

--- BE/test/index.js
+++ BE/test/index.js
@@ -1,11 +1,11 @@
 const { createAdmins } = require("./create-admin");
-const { createDummyShop } = require("./create-dummyshop");
-const { createDummyFood } = require("./create-dummyfood");
+const { createDefaultShop } = require("./create-default-shop");
+const { createDefaultFood } = require("./create-default-food");
 
 async function setData() {
-  // await createAdmins();
-  // await createDummyShop();
-  await createDummyFood();
+  await createAdmins();
+  await createDefaultShop();
+  // await createDefaultFood();
 }
 
 module.exports = { setData };
