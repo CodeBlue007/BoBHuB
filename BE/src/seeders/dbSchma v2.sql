39cd8260b84e1d20cce9fb73f8e66175d0aa29fc
상준 안 <ahosang0056@gmail.com>
2022-12-28 15:51:37 +0900

    refactor(DDL) : pick casecade 추가

--- BE/src/seeders/dbSchma v2.sql
+++ BE/src/seeders/dbSchma v2.sql
@@ -112,8 +112,8 @@ CREATE TABLE IF NOT EXISTS `pick` (
   `updatedAt` DATETIME, 
   `deletedAt` DATETIME, 
   PRIMARY KEY (`pickId`, `userId`, `partyId`), 
-  FOREIGN KEY (`userId`) REFERENCES `user` (`userId`), 
-  FOREIGN KEY (`partyId`) REFERENCES `party` (`partyId`),
+  FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE, 
+  FOREIGN KEY (`partyId`) REFERENCES `party` (`partyId`) ON DELETE CASCADE ON UPDATE CASCADE,
   UNIQUE `pick_userId_partyId_unique` (`userId`, `partyId`)
 );
 CREATE TABLE IF NOT EXISTS `completedParty` (
