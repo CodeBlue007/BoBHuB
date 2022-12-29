74f0619d6ee5bd62ed61bf9edd4750d1ae2d4873
dongjooyun <dongjooyun@khu.ac.kr>
2022-12-29 19:18:01 +0900

    Update: 이메일 인증코드 체크 및 css 개선 필요

--- FE/src/pages/Register/Api/registerAPI.ts
+++ FE/src/pages/Register/Api/registerAPI.ts
@@ -1,71 +1,16 @@
-import { regReqType } from '../types/regType';
-import axios from 'axios';
+import * as API from '../../../api/API';
+import { PostEmail, PostEmailCode } from '../types/regType';
 
-// api 로 POST 요청 (/endpoint 로, JSON 데이터 형태로 요청)
-const postRegisterData = async (regData: regReqType) => {
-  console.log(`POST 요청 데이터: ${JSON.stringify(regData)}`);
-
-  //   try {
-  //     // const { reqData } = await axios.post<regReqType>('http://localhost:3000/user/join', regData, {
-  //     //   headers: {
-  //     //     'Content-Type': 'application/json',
-  //     //     Accept: 'application/json',
-  //     //   },
-  //     // });
-  //     // console.log(JSON.stringify(reqData, null, 4));
-  //     // return reqData;
-  //   } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       console.log('ERROR: ', error.message);
-  //       return error.message;
-  //     } else {
-  //       console.log('unexpected error: ', error);
-  //       return 'An unexpected error occurred';
-  //     }
-  //   }
-
-  await axios
-    .post('http://localhost:3000/user/join', regData)
-    .then(() => console.log('post success'))
-    .catch((error) => {
-      if (error.response) {
-        // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
-        const status = error.response.status;
-        if (status >= 500) {
-          console.log('Internal Server Error');
-        } else if (status >= 400) {
-          console.log('Request Failed');
-        } else if (status >= 200) {
-          console.log('OK');
-        }
-      } else if (error.request) {
-        // 요청이 이루어 졌으나 응답을 받지 못했습니다.
-        // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
-        // Node.js의 http.ClientRequest 인스턴스입니다.
-        console.log(error.request);
-      } else {
-        // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
-        console.log('Error', error.message);
-      }
-      //   console.log(error.config);
-    });
-
-  //   const res = await fetch(apiUrl, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-  //     },
-  //     body: bodyData,
-  //   });
-
-  // 응답 코드가 4XX 계열일 때 (400, 403 등)
-  //   if (!res.ok) {
-  //     const errorContent = await res.json();
-  //     const { reason } = errorContent;
-
-  //     throw new Error(reason);
-  //   }
+// 인증 메일 전송 api 요청
+const postEmail = async (regEmail: PostEmail) => {
+  const resEmail = await API.post('api/utils/email/send-code', regEmail);
+  return resEmail;
 };
 
-export default postRegisterData;
+// 이메일 인증 코드 api 요청
+const postEmailCode = async (regEmailCode: PostEmailCode) => {
+  const resEmailCode = await API.post('api/utils/email/check-code', regEmailCode);
+  return resEmailCode;
+};
+
+export { postEmail, postEmailCode };
--- FE/src/pages/Register/types/regType.ts
+++ FE/src/pages/Register/types/regType.ts
@@ -3,37 +3,30 @@ export interface regFormProps {
     name: string;
     nickname: string;
     email: string;
-    confirmNum: string;
+    emailCode: string;
     password: string;
     passwordCheck: string;
     phone: string;
     track: string;
-    // generation: number;
     generation: string;
   }) => void;
 }
 
 export type regReqType = {
   track: string;
-  // generation: number;
   generation: string;
   name: string;
   email: string;
   nickname: string;
   password: string;
   phone: string;
-  //   role: string;
 };
 
-// export type regForm = {
-//   name: string;
-//   nickname: string;
-//   password: string;
-//   passwordCheck: string;
-//   phone: string;
-//   email: string;
-//   confirmNum: string;
-//   track: string;
-// generation: number;
-// generation: string;
-// };
+export type PostEmail = {
+  email: string;
+};
+
+export type PostEmailCode = {
+  email: string;
+  emailCode: string;
+};
